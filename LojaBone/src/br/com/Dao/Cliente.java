a94c94b8efe8ead89a8b9b5efd9b86bb34b71bac
WILDMAX <WILDMAX@WILDMAX-PC>
2016-10-30 15:48:00 -0200

    Projeto LTP4 Primeiro Passo

---
+++ LojaBone/src/br/com/Dao/Cliente.java
@@ -0,0 +1,6 @@
+package br.com.Dao;
+
+public class Cliente {
+	
+
+}
---
+++ LojaBone/src/br/com/banco/ConexaoBanco.java
@@ -0,0 +1,22 @@
+package br.com.banco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBanco {
+	
+	public static Connection getConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/projeto?user=root&password=1234");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e){
+			e.printStackTrace();
+		
+		}
+		return null;		
+	}
+
+}
